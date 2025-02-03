c02ea1d48a12b0ac7d5a7e9db35a5cf0765230a0
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-02-03 23:44:14 +0530

    terraform-mul env

---
+++ tfvars/var.tf
@@ -0,0 +1,25 @@
+variable "project"{
+    default = "expense"
+}
+variable "environment" {
+    
+}
+variable "instances" {
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "zone_id" {
+    default = "Z047033534XH9OO4TGU3"
+}
+
+variable "domain_name" {
+    default = "somisettibhavya.life"
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "expense"
+        Terraform = "true"
+    }
+}
